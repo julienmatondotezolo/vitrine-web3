bd11e1126ebe8178d2b7ab00cb6a9167d309352c
machai <chaimae.benhammou@student.ehb.be>
2021-11-22 14:25:33 +0100

    google  auth busy

--- api/src/routes/auth/google/GooglePassport.js
+++ api/src/routes/auth/google/GooglePassport.js
@@ -2,25 +2,12 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const passport =require("passport")
 
 
-module.exports = async function (passport) {
-        
-passport.use(new GoogleStrategy({
-        clientID:"here use the clientID given by google",
-        clientSecret:"here use the client secret given by google",
-        callbackURL: "http://localhost:5000/google/callback",
-        passReqToCallback   : true
-    },
-    function(request, accessToken, refreshToken, profile, done) {
-            return done(null, profile);
-    }
-));
-
-passport.serializeUser(function(user, done) {
-        done(null, user);
-    });
-    
-    passport.deserializeUser(function(user, done) {
-            done(null, user);
-    });
-    
-};
+router.get('/profile', passport.authenticate('google', { scope: ['profile'] }))
+router.get(
+        '/auth/google/callback',
+        passport.authenticate('google', { failureRedirect: '/signin' }),
+        async function (_req, res, _next) {
+          res.redirect('/')
+        }
+      )
+      
