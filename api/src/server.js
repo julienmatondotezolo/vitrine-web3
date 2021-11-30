dbed0734c8f53d1be1050509186e41ba749e66cd
machai <chaimae.benhammou@student.ehb.be>
2021-11-30 11:29:11 +0100

    redundant next() removed

--- api/src/server.js
+++ api/src/server.js
@@ -31,7 +31,6 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
-  next();
 });
 
 app.use(cors({
