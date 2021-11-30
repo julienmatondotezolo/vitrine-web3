026ca4251f7e9fae4bc54803fb250ba15506f94c
machai <chaimae.benhammou@student.ehb.be>
2021-11-30 19:49:23 +0100

    try ensureauthenticated message

--- api/src/routes/auth/ensureAuthenticated.js
+++ api/src/routes/auth/ensureAuthenticated.js
@@ -1,9 +1,9 @@
 module.exports = {
   ensureAuthenticated: function (req, res, next) {
-    console.log(req.isAuthenticated);
+    console.log(req.isAuthenticated());
     if (req.isAuthenticated()) {
       return next();
     }
-    res.sendCustomStatus(401, "Please log in to view that resource");
+    res.sendCustomStatus(401, "Please log in");
   },
 };
