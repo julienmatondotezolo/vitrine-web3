0e3f3b5073f9f6580603dcdaaacd8b5be171adba
machai <chaimae.benhammou@student.ehb.be>
2021-12-06 11:55:16 +0100

    headers cors changed

--- api/src/server.js
+++ api/src/server.js
@@ -9,8 +9,8 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 const pool = require("./db/db");
 const session = require("express-session");
-const pgSession = require('connect-pg-simple')(session);
-const sessionPool = require('pg').Pool;
+const pgSession = require("connect-pg-simple")(session);
+const sessionPool = require("pg").Pool;
 
 const cookieParser = require("cookie-parser");
 
@@ -31,10 +31,7 @@ const getProjectsUser = require("./routes/projects/get-projects-user");
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Origin",
-    "http://www.vitrine-finalshow.be/"
-  );
+  res.header("Access-Control-Allow-Origin", "http://www.vitrine-finalshow.be/");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
   res.header(
     "Access-Control-Allow-Headers",
@@ -42,13 +39,6 @@ app.use(function (req, res, next) {
   );
   next();
 });
-app.options(
-  "*",
-  cors({
-    origin: "http://www.vitrine-finalshow.be/",
-    optionsSuccessStatus: 200,
-  })
-);
 
 app.use(
   cors({
@@ -57,7 +47,6 @@ app.use(
       "http://api.vitrine-finalshow.be/",
     ],
     methods: "GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH",
-    preflightContinue: true,
     credentials: true,
   })
 );
@@ -74,22 +63,23 @@ const sessionDBaccess = new sessionPool({
   // host:"ec2-54-171-25-232.eu-west-1.compute.amazonaws.com",
   // port: "5432",
   // database: "ddo8518ajthv3s",
-  connectionString:"PostgreSQL://qrucjvzwvowlfs:2210978d2cf983a3c38894effb0e245e0866f4dde585ec11cc4ea59b129fd835@ec2-54-171-25-232.eu-west-1.compute.amazonaws.com/ddo8518ajthv3s?statusColor=&enviroment=production&name=vitrine_backend&tLSMode=0&usePrivateKey=false&safeModeLevel=0&advancedSafeModeLevel=0&driverVersion=0",
+  connectionString:
+    "PostgreSQL://qrucjvzwvowlfs:2210978d2cf983a3c38894effb0e245e0866f4dde585ec11cc4ea59b129fd835@ec2-54-171-25-232.eu-west-1.compute.amazonaws.com/ddo8518ajthv3s?statusColor=&enviroment=production&name=vitrine_backend&tLSMode=0&usePrivateKey=false&safeModeLevel=0&advancedSafeModeLevel=0&driverVersion=0",
   ssl: {
     rejectUnauthorized: false,
   },
-})
+});
 
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(
   session({
     store: new pgSession({
       pool: sessionDBaccess,
-      tableName: 'session',
-  }),
+      tableName: "session",
+    }),
     secret: process.env.SECRET,
     resave: false,
-    unset: 'destroy',
+    unset: "destroy",
     cookie: {
       domain: "http://www.vitrine-finalshow.be/",
       maxAge: oneDay,
