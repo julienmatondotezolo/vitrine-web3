83cd294aa537c9c75b52138d06e2640b04067870
machai <chaimae.benhammou@student.ehb.be>
2021-12-06 10:26:44 +0100

    change sessionId to session

--- api/src/routes/auth/login.js
+++ api/src/routes/auth/login.js
@@ -6,12 +6,12 @@ router.post('/',
   passport.authenticate('local', { failureMessage: 'You have nog been logged in', failureFlash: true ,successFlash: 'You have succesfully logged in!'}),
   function(req, res) {
     console.log(JSON.stringify(req.session))
-    res.send(req.sessionID)
+    res.send(req.session)
   });
 
 router.get("/", (req, res) => {
   if (req.isAuthenticated()) {
-    res.send(req.sessionID)
+    res.send(req.session)
     return;
   }
 
