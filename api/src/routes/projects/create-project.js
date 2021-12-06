b88aeded987ad2ad21bba089ad3caf5bd015afdd
machai <chaimae.benhammou@student.ehb.be>
2021-12-06 13:41:37 +0100

    update has to work

--- api/src/routes/projects/create-project.js
+++ api/src/routes/projects/create-project.js
@@ -6,9 +6,9 @@ router.post("/", async (req, res) => {
   let { projectName, description, url, images, cluster } = req.body;
   let userId = req.session.passport.user.userid;
 
+  console.log("user id:   ============="+userId)
   let values = [projectName, description, url, images, cluster, userId];
 
-  if (check(cluster)) {
     try {
       const newProject = await pool.query(
         "INSERT INTO projects(name, description, url, images, cluster_id, user_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
@@ -20,18 +20,7 @@ router.post("/", async (req, res) => {
       console.error(err.message);
       res.sendCustomStatus(500);
     }
-  } else {
-    res.sendCustomStatus(400);
-  }
-
-  function check(cluster) {
-    let clusters = ["web", "mobile", "motion", "ar", "digital-making"];
-
-    if (clusters.includes(cluster)) {
-      return true;
-    }
-    return false;
-  }
+  
 });
 
 module.exports = router;
