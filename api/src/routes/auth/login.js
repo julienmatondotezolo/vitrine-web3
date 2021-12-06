7245d7505f8af0fc7d541400b6e7d4ce874bfa54
machai <chaimae.benhammou@student.ehb.be>
2021-12-06 14:48:52 +0100

    login should work

--- api/src/routes/auth/login.js
+++ api/src/routes/auth/login.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
-router.post("/", (req, res, next) => {
-  passport.authenticate("local", {
-    successRedirect: "/home",
-    failureRedirect: "/login/error",
-    failureFlash: true,
-  })(req, res, next);
-});
+router.post('/', 
+  passport.authenticate('local', { failureMessage: 'You have nog been logged in', failureFlash: true ,successFlash: 'You have succesfully logged in!',session:true}),
+  function(req, res) {
+    res.send(req.session)
+  });
+
 router.get("/", (req, res) => {
   if (req.isAuthenticated()) {
     res.send(req.session)
