b0aa4f84f4978d0983bd7ea31e01c757d27d76d0
machai <chaimae.benhammou@student.ehb.be>
2021-12-06 12:15:12 +0100

    headers chg

--- api/src/server.js
+++ api/src/server.js
@@ -33,10 +33,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "http://www.vitrine-finalshow.be/");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+  res.header("Access-Control-Allow-Headers","Origin, Content-Type, Authorization");
   next();
 });
 
